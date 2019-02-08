4e5cce3a26ad3a2235ad9bf06c0515cf7ca10583
Vitaly Goncharenko <vitaliy.goncharenko@hotmail.com>
2019-02-08 22:02:32 +0800

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,4 +3,8 @@ import React from 'react';
 // Import Styled Components
 import { FooterContainer } from './Footer.atoms';
 
-export default () => <FooterContainer>&nbsp;</FooterContainer>;
+export default () => (
+  <FooterContainer>
+    <div>Technosoft</div>
+  </FooterContainer>
+);
