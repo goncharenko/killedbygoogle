fe4abb655f20f244a15a38bb98e6195e7451ef39
Vitaly Goncharenko <vitaliy.goncharenko@hotmail.com>
2019-02-08 21:47:40 +0800

    fix

--- src/components/GreatQuotes.jsx
+++ src/components/GreatQuotes.jsx
@@ -3,4 +3,4 @@ import React from 'react';
 // Import Styled Components
 import { Quote } from './Header.atoms';
 
-export default () => <Quote>Projects, that are not good for us</Quote>;
+export default () => <Quote>&quot;Projects, that are not good for us&quot;</Quote>;
